b1d4a24e42870d96934a6f3a19f0459e14ac04dc
Vincent Jareno <vincent@hybridanchor.com>
2025-04-29 03:18:48 +0800

    Added second section markup and styles

---
+++ views/related.php
@@ -0,0 +1,38 @@
+<section class="page-section">
+  <div class="container">
+    <div class="page-section__header center">
+      <h2 class="page-section__heading">Taste the Colours</h2>
+    </div>
+
+    <div class="card__deck">
+
+      <div class="card">
+        <div class="card__img">
+          <img src="/public/images/colour-red.webp" alt="Taste of Colours - Red">
+        </div>
+        <div class="card__content">
+          <h3>Red</h3>
+          <p>Red foods remind us of berries and soft fruits, so we anticipate a sweet taste.</p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card__img">
+          <img src="/public/images/colour-green.webp" alt="Taste of Colours - Green">
+        </div>
+        <div class="card__content">
+          <h3>Green</h3>
+          <p>Fresh, zingy green colours are reminiscent of unripe fruit, promising sour or acid flavours</p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card__img">
+          <img src="/public/images/colour-white.webp" alt="Taste of Colours - White">
+        </div>
+        <div class="card__content">
+          <h3>White</h3>
+          <p>White foods evoke memories of salt and salty flavours, driving the expectation of a savoury treat.</p>
+        </div>
+
+    </div>
+  </div>
+</section>
